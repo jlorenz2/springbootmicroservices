5e551355144736e7ec0f514e7940944c5f9da5d0
jorge_000 <jorge_000@usuario>
2020-11-29 17:51:35 +0100

    Customize validation

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -2,11 +2,16 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 	private Integer id;
 	
+	@Size(min = 2, message = "User name must be 2 characters minimum")
 	private String  name;
 	
+	@Past
 	private Date birthDate;
 
 	public Integer getId() {
